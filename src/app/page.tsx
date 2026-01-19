ecaaccead55f141818ff685b68c8b90f46defe78
juanvelozo <juanvelozomoreno18@gmail.com>
2026-01-19 19:14:13 -0300

    corrige home para optimizar la llamada a la api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,12 @@ import { getExchangeInfoWithTiming } from '@/services/binance.service';
 import { ResponseTime } from './components/ResponseTime/ResponseTime';
 import { SymbolSelect } from './components/SymbolSelect';
 
-async function SymbolSelectWithData() {
+async function DataFetcher() {
   const { data: exchangeInfo, responseTime } = await getExchangeInfoWithTiming();
 
   return (
     <>
-      <div className="flex items-center justify-between">
-        <h1 className="text-3xl font-bold text-zinc-900 dark:text-white">
-          Elegí un symbol
-        </h1>
-        <ResponseTime responseTime={responseTime} />
-      </div>
-
+      <ResponseTime responseTime={responseTime} />
       <SymbolSelect
         symbols={exchangeInfo.symbols}
         placeholder={`Buscá un símbolo (${exchangeInfo.symbols.length} disponibles)`}
@@ -26,21 +20,17 @@ async function SymbolSelectWithData() {
 export default async function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-4xl flex-col items-center justify-start py-12 px-6 bg-white dark:bg-black">
+      <main className="flex min-h-screen w-full flex-col items-center justify-start py-12 px-6 bg-white dark:bg-black">
         <div className="w-full space-y-6">
-          <Suspense
-            fallback={
-              <>
-                <div className="flex items-center justify-between">
-                  <h1 className="text-3xl font-bold text-zinc-900 dark:text-white">
-                    Cargando...
-                  </h1>
-                </div>
-                <SymbolSelect loading />
-              </>
-            }
-          >
-            <SymbolSelectWithData />
+          {/* Título fuera del Suspense para LCP rápido */}
+          <h1 className="text-3xl font-bold text-zinc-900 dark:text-white">
+            Orderbook Viewer
+          </h1>
+          <h3 className="text-md  text-zinc-900 dark:text-white">
+            Esta herramienta te permite ver el order book de un símbolo (crypto currency) en tiempo real.
+          </h3>
+          <Suspense fallback={<SymbolSelect loading />}>
+            <DataFetcher />
           </Suspense>
         </div>
       </main>
